fa8e0257f4f31f428af014d793b28114ca7b862e
Timothy Wood <codearachnid@gmail.com>
2023-03-03 10:42:04 -0500

    init boilerplate

---
+++ includes/class-qlbaf-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://alwayscurious.co
+ * @since      1.0.0
+ *
+ * @package    Qlbaf
+ * @subpackage Qlbaf/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Qlbaf
+ * @subpackage Qlbaf/includes
+ * @author     Timothy Wood @codearachnid <info@alwayscurious.co>
+ */
+class Qlbaf_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-qlbaf-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://alwayscurious.co
+ * @since      1.0.0
+ *
+ * @package    Qlbaf
+ * @subpackage Qlbaf/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Qlbaf
+ * @subpackage Qlbaf/includes
+ * @author     Timothy Wood @codearachnid <info@alwayscurious.co>
+ */
+class Qlbaf_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
